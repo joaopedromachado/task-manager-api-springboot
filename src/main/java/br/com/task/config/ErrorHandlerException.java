d3c4e80eb45477c580462d8729eac577c67075be
João Pedro Machado <joaopemachado2200@gmail.com>
2023-06-23 03:58:48 -0300

    add handler errors

--- src/main/java/br/com/task/config/ErrorHandlerException.java
+++ src/main/java/br/com/task/config/ErrorHandlerException.java
@@ -1,23 +1,38 @@
 package br.com.task.config;
 
 import br.com.task.resource.exception.StandardError;
+import br.com.task.service.exception.TaskDeletionException;
 import br.com.task.service.exception.TaskNotFoundException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
+@Configuration
 @ControllerAdvice
 public class ErrorHandlerException {
 
-    @ExceptionHandler({TaskNotFoundException.class})
-    public ResponseEntity<Void> handlerArgsNotFoundException() {
-        // TODO Implementar handler
-        return null;
+    @ExceptionHandler(value = {TaskNotFoundException.class, TaskDeletionException.class})
+    public ResponseEntity<StandardError> handlerArgsNotFoundException(TaskNotFoundException e, WebRequest request) {
+        StandardError error = new StandardError.Builder()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .timestamp(System.currentTimeMillis())
+                .message(e.getMessage())
+                .path(request.getDescription(false))
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({Exception.class})
-    public ResponseEntity<StandardError> globalHandlerArgsException() {
-        // TODO Implementar handler
-        return null;
+    public ResponseEntity<StandardError> globalHandlerArgsException(Exception e) {
+        StandardError error = new StandardError.Builder()
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .timestamp(System.currentTimeMillis())
+                .message(e.getMessage())
+                .path(e.getClass().getPackageName())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- src/main/java/br/com/task/resource/exception/StandardError.java
+++ src/main/java/br/com/task/resource/exception/StandardError.java
@@ -1,11 +1,47 @@
 package br.com.task.resource.exception;
 
-import java.time.LocalDateTime;
-
 public class StandardError {
-
     private int statusCode;
-    private LocalDateTime timestamp;
+    private long timestamp;
     private String message;
     private String path;
+
+    public StandardError(Builder builder) {
+        this.statusCode = builder.statusCode;
+        this.timestamp = builder.timestamp;
+        this.message = builder.message;
+        this.path = builder.path;
+    }
+
+    public static class Builder {
+        private int statusCode;
+        private long timestamp;
+        private String message;
+        private String path;
+
+        public Builder statusCode(int statusCode) {
+            this.statusCode = statusCode;
+            return this;
+        }
+
+        public Builder timestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder path(String path) {
+            this.path = path;
+            return this;
+        }
+
+        public StandardError build() {
+            return new StandardError(this);
+        }
+    }
+
 }
